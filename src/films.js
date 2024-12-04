d5560ee3b392f38fe817e3eaa406e0cdc83876cb
Alberto Basanta <dasantades@gmail.com>
2024-12-04 08:17:24 +0100

    ejercicios del 1 al 4

--- src/films.js
+++ src/films.js
@@ -1,23 +1,62 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result = array.map((movie) => movie.director);
+
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter((movie) => movie.director === director);
+
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
+  let moviesFromDirector = array.filter((movie) => movie.director === director);
+  console.log("EXERCICE 3 ->", moviesFromDirector);
+
+  let sumaNotas = moviesFromDirector.reduce((acumulador, movie) => acumulador + movie.score, 0);
+  console.log("EXERCICE 3 ->", sumaNotas);
+
   
+  let result = sumaNotas / moviesFromDirector.length;
+
+  console.log("EXERCICE 3 ->", result);
+  return result;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let titulos = array.map((movie) => movie.title);
+
+  console.log("EXERCICE 4 ->", titulos);
+
+  // let titulosOrdenados = titulos.sort();
+
+  // console.log("EXERCICE 4 ->", titulosOrdenados);
+
+let titulosOrdenados = titulos.sort((a, b) => {
+  // Ordenar correctamente por los números al principio de las cadenas
+  const numA = parseInt(a.match(/^\d+/)); // Extrae el número al inicio de 'a'
+  const numB = parseInt(b.match(/^\d+/)); // Extrae el número al inicio de 'b'
+
+  // Si hay números, compáralos; si no, ordena como cadenas normales
+  if (numA && numB) {
+    return numA - numB; // Orden numérico ascendente
+  } else {
+    return a.localeCompare(b); // Orden alfabético como fallback
+  }
+});  console.log("EXERCICE 4 ->", titulosOrdenados);
+
+  if (titulosOrdenados.length > 20) {
+    return titulosOrdenados.slice(0, 20);
+  }
+
+  return titulosOrdenados;
 }
 
 // Exercise 5: Order by year, ascending
